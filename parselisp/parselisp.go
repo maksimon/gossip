1309991fc7d7686cb5e351fb5d359cc0fbd0b25f
maksimon <vernikovskiy.maksim@gmail.com>
2015-06-02 01:53:26 -0400

    decided on types, mostly done with frontend

--- parselisp/parselisp.go
+++ parselisp/parselisp.go
@@ -6,23 +6,51 @@ import(
   "gossip/types"
 )
 
-/*
+var tokenRegexp *regexp.Regexp = regexp.MustCompile(`(\(|\)|[^ ()]+)`)
+var raw_source string = "(print (append your total is :(+ 1 2 3 (+ 4 5 6 8) (* 3 4) ) ) )"
+//func ParseLisp(/*raw_source string*/) types.LispElement {
+//  helper := func () types.LispElement {
+//    currentElement := types.LispElement{ "", &[]types.LispElement{}, types.LispList }
+//    return currentElement
+//  }
+//  return helper();
+//}
+//
+//func main() {
+//  fmt.Println(tokenRegexp.FindAllString(raw_source,-1)[1])
+//  lt := types.LispElement{ "-", nil ,types.LispFunction}
+//  fmt.Printf("The value is %d\n", lt.Type)
+//  //return lt
+//  //ParseLisp("(print (append your total is :(+ 1 2 3 (+ 4 5 6 8) (* 3 4) ) ) )")
+//  le2 := ParseLisp()
+//  fmt.Printf("The valie is also %d\n", le2.Type)
+//}
 
-func makeRootNode syntaxNode {
+func ParseLisp(rawSource string) types.LispList {
+  ret := types.NewList()
+  return *ret
 
 }
 
-
-func countTokens(raw_source string) {
-
-
-}*/
+func parseLispHelper(rawSource *string, index int) types.LispList, int {
+  retList := types.NewList()
+  for {
+    switch *rawSource[index] {
+      default:
+        retList.Children = retList.Children.append(types.NewNumber(*rawSource[index]))
+      case "(":
+        child, index := parseLispHelper(rawSource, index)
+        retList.Children = retList.Children.append(child)
+      case ")":
+        return retList, index
+    }
+    index += 1
+  }
+}
 
 func main() {
-  re := regexp.MustCompile(`(\(|\)|[^ ()]+)`)
-  fmt.Println(re.FindAllString("(print (append your total is :(+ 1 2 3 (+ 4 5 6 8) (* 3 4) ) ) )",-1)[3])
-  lt := types.LispElem{ "-", nil ,types.LispFunction}
-  fmt.Printf("The value is %d\n", lt.Type)
-
+  lp := *types.NewNumber("+")
+  ll := ParseLisp("hey")
+  fmt.Printf("The type is %d\n", lp.Type())
+  fmt.Printf("The length is %d\n", len(ll.Children))
 }
-
--- types/types.go
+++ types/types.go
@@ -2,15 +2,51 @@ package types
 
 type LispType int
 
-type LispElem struct {
-  Value string
-  Children *[]LispElem
-  Type LispType
+const (
+  NumberType LispType = iota
+  FunctionType LispType = iota 
+  ListType  LispType = iota
+)
+
+type LispElement interface {
+  Value() string
+  Type() int
 }
 
-const (
-  LispNumber LispType = iota
-  LispFunction LispType = iota 
-  LispArray  LispType = iota
-)
+type LispPrimative struct {
+  value string
+  lispType LispType
+}
+
+func (LispPrimative *LispPrimative) Value() string {
+  return LispPrimative.value
+}
+
+func (LispPrimative *LispPrimative) Type() LispType {
+  return LispPrimative.lispType
+}
+
+func NewFunction(functionName string) *LispPrimative {
+  return &LispPrimative{functionName, FunctionType}
+}
+
+func NewNumber(numberString string) *LispPrimative {
+  return &LispPrimative{numberString, NumberType}
+}
+
+type LispList struct {
+  Children []LispElement
+}
+
+func (list *LispList) Value() string {
+  return "(list)"
+}
+
+func (list *LispList) Type() LispType {
+  return ListType
+}
+
+func NewList() *LispList {
+  return &LispList{ make([]LispElement, 0) }
+}
 
