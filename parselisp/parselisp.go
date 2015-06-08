a0ae081b65a4497508dce5119c9ccc6fc7979008
maksimon <zakaden@gmail.com>
2015-06-07 20:14:58 -0400

    Parsing numbers and lists seems to work

--- parselisp/parselisp.go
+++ parselisp/parselisp.go
@@ -6,51 +6,46 @@ import(
   "gossip/types"
 )
 
-var tokenRegexp *regexp.Regexp = regexp.MustCompile(`(\(|\)|[^ ()]+)`)
-var raw_source string = "(print (append your total is :(+ 1 2 3 (+ 4 5 6 8) (* 3 4) ) ) )"
-//func ParseLisp(/*raw_source string*/) types.LispElement {
-//  helper := func () types.LispElement {
-//    currentElement := types.LispElement{ "", &[]types.LispElement{}, types.LispList }
-//    return currentElement
-//  }
-//  return helper();
-//}
-//
-//func main() {
-//  fmt.Println(tokenRegexp.FindAllString(raw_source,-1)[1])
-//  lt := types.LispElement{ "-", nil ,types.LispFunction}
-//  fmt.Printf("The value is %d\n", lt.Type)
-//  //return lt
-//  //ParseLisp("(print (append your total is :(+ 1 2 3 (+ 4 5 6 8) (* 3 4) ) ) )")
-//  le2 := ParseLisp()
-//  fmt.Printf("The valie is also %d\n", le2.Type)
-//}
+var tokenRegexp *regexp.Regexp = regexp.MustCompile(`(\(|\)|[^() ]+)`)
+//var raw_source string = "(print (append your total is :(+ 1 2 3 (+ 4 5 6 8) (* 3 4) ) ) )"
 
 func ParseLisp(rawSource string) types.LispList {
-  ret := types.NewList()
+  parsedSource := tokenRegexp.FindAllString(rawSource, -1)
+  ret , _ := parseLispHelper((&parsedSource), 0)
   return *ret
-
 }
 
-func parseLispHelper(rawSource *string, index int) types.LispList, int {
+func parseLispHelper(rawSource *[]string, cursor int) (*types.LispList, int) {
   retList := types.NewList()
+  cursor += 1 // skipping over first '('
+  var cursorValue string
   for {
-    switch *rawSource[index] {
+    cursorValue = string((*rawSource)[cursor])
+    switch cursorValue {
       default:
-        retList.Children = retList.Children.append(types.NewNumber(*rawSource[index]))
+         retList.Append(types.NewNumber(cursorValue))
       case "(":
-        child, index := parseLispHelper(rawSource, index)
-        retList.Children = retList.Children.append(child)
+        var child *types.LispList
+        child, cursor = parseLispHelper(rawSource, cursor)
+        retList.Append(child)
       case ")":
-        return retList, index
+        return retList, cursor
     }
-    index += 1
+    cursor += 1
   }
 }
 
 func main() {
   lp := *types.NewNumber("+")
-  ll := ParseLisp("hey")
+  ll := ParseLisp("(1 2 (8 9 10) 3)")
   fmt.Printf("The type is %d\n", lp.Type())
   fmt.Printf("The length is %d\n", len(ll.Children))
+  for i := 0; i < len(ll.Children) ; i++ {
+    fmt.Printf("Value: %s Type: %d", ll.Children[i].Value(), ll.Children[i].Type())
+    if (ll.Children[i].Type() == types.ListType) {
+      listChild := ll.Children[i].(types.LispList)
+      fmt.Printf(" Length: %d", len(listChild.Children))
+    }
+    fmt.Printf("\n")
+  }
 }
